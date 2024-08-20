1a925d9a339701cafe56f28ba7b5d7a440a4b059
afeefaam <afeefam.am@gmail.com>
2024-08-19 22:20:24 -0400

    Add files via upload

---
+++ t01.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------------------
+[This program outputs sentences]
+-------------------------------------------------------
+Author:  Afeefa Malik
+ID:      169060299
+Email:   mali0299@mylaurier.ca
+__updated__ = "2023-09-25"
+-------------------------------------------------------
+"""
+
+print('What is a "program"?')
+print("A program is a sequence of instructions that specifies how to perform a computation. The details look different in different languages, but a few basic instructions appear in just about every language: input: Get data from the keyboard, a file, or some other device.output: Display data on the screen or send data to a file or other device.math: Perform basic mathematical operations like addition and multiplication.conditional execution: Check for certain conditions and execute the appropriate code.repetition: Perform some action repeatedly, usually with some variation.")
+print('''Believe it or not, that's pretty much all there is to it.''')
+print("""You have enemies? Good. That means you've stood up for something, sometime in your life." Winston Churchill""")
