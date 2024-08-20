cf097a7cd69bded119db4b1176b03d9a6d6f5968
afeefaam <afeefam.am@gmail.com>
2024-08-19 22:50:57 -0400

    Add files via upload

---
+++ t04.py
@@ -0,0 +1,14 @@
+"""
+-------------------------------------------------------
+[lab 1, task 4]
+-------------------------------------------------------
+Author:  Afeefa
+ID:      169060299
+Email:   mali0299@mylaurier.ca
+__updated__ = "2023-09-11"
+-------------------------------------------------------
+"""
+# Imports
+name = input("Please enter your name: ")
+print("Pleased to meet you ")
+print(name)
